7940238f3ea94e4b59462b1b1e03e20f1c393720
tsukiji <tsukiji@mmi-sc.co.jp>
2016-02-22 02:38:48 +0900

    会社名表示追加

---
+++ app/src/main/java/mmi_sc/co/jp/nosmoking/AboutActivity.java
@@ -0,0 +1,26 @@
+package mmi_sc.co.jp.nosmoking;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.ImageView;
+
+public class AboutActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        ImageView iv = (ImageView)findViewById(R.id.logoImageView);
+        iv.setImageResource(R.drawable.mmi_logo);
+//        setContentView(iv);
+
+    }
+
+}
--- app/src/main/java/mmi_sc/co/jp/nosmoking/MainActivity.java
+++ app/src/main/java/mmi_sc/co/jp/nosmoking/MainActivity.java
@@ -1,5 +1,6 @@
 package mmi_sc.co.jp.nosmoking;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -42,6 +43,10 @@ public class MainActivity extends AppCompatActivity {
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
+            return true;
+        } else if (id == R.id.action_about) {
+            Intent intent = new Intent(this, AboutActivity.class);
+            startActivity(intent);
             return true;
         }
 
