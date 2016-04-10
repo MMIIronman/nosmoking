f055df5e1ce6377a5d2cb7943ed9ffd8a4b14546
MMIIronman <k-hijiya@mmi-sc.co.jp>
2016-04-11 00:50:33 +0900

    ・画面を作成し、リセットボタンを追加

--- app/src/main/java/jp/co/mmi_sc/nosmoking/MainActivity.java
+++ app/src/main/java/jp/co/mmi_sc/nosmoking/MainActivity.java
@@ -101,6 +101,8 @@ public class MainActivity extends AppCompatActivity {
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
             Toast.makeText(this, "Select menu is Setting", Toast.LENGTH_SHORT).show();
+            Intent intent = new Intent(this, SettingMenuActivity.class);
+            startActivity(intent);
             return true;
         } else if (id == R.id.action_about) {
             Intent intent = new Intent(this, AboutActivity.class);
