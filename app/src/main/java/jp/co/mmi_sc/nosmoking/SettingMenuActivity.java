acb83e6d4d2a4fe3e9e3e6405164c5816d1e53f6
MMIIronman <k-hijiya@mmi-sc.co.jp>
2016-04-11 23:01:46 +0900

    ・リセット確認ダイアログを追加

--- app/src/main/java/jp/co/mmi_sc/nosmoking/SettingMenuActivity.java
+++ app/src/main/java/jp/co/mmi_sc/nosmoking/SettingMenuActivity.java
@@ -1,7 +1,9 @@
 package jp.co.mmi_sc.nosmoking;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.widget.Button;
@@ -19,6 +21,7 @@ public class SettingMenuActivity  extends AppCompatActivity {
     private TextView mSettingMenuIDText;
     private Button mButtonResetCount;
     private Button mButtonFelica;
+    private AlertDialog mConfirmDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,18 +37,30 @@ public class SettingMenuActivity  extends AppCompatActivity {
         mSettingMenuIDText.setText(R.string.message_activity_setting_menu);
 
         mButtonResetCount = (Button)findViewById(R.id.button_resetcount);
-
         mButtonResetCount.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                if (v == mButtonResetCount) {
-                    mConfig.initMyConfig();
-                    Toast toast = Toast.makeText(SettingMenuActivity.this, R.string.message_reset_count, Toast.LENGTH_LONG);
-                    toast.show();
-                }
+                    mConfirmDialog.show();
             }
         });
 
         mButtonFelica = (Button)findViewById(R.id.button_felica);
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle(R.string.button_reset_count);
+        builder.setMessage(R.string.message_confirm_dlg);
+        builder.setPositiveButton(R.string.button_ok, new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int which) {
+                mConfig.initMyConfig();
+                Toast toast = Toast.makeText(SettingMenuActivity.this, R.string.message_reset_count, Toast.LENGTH_LONG);
+                toast.show();
+            }
+        });
+        builder.setNegativeButton(R.string.button_no, new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int which) {
+            }
+        });
+
+        mConfirmDialog = builder.create();
     }
 
     @Override
